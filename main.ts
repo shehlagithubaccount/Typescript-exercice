aaf04b1eadfba4fae87aad65b18effd0eac7509b
shehlagithubaccount <166911610+shehlagithubaccount@users.noreply.github.com>
2024-04-13 15:05:01 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,19 @@
+let x : number = 4;
+let y : number = 5;
+let proper : number = x+y;
+console.log(proper);
+
+let z : number = 4;
+let q : number = 5;
+let devide : number = x/y;
+console.log(devide);
+
+
+
+
+
+
+
+
+
+
